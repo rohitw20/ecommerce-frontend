5943f9820b5f1e0593bb57bd3d5f0489b33c7e80
rohitw20 <rohit.w@aikyamedge.com>
2024-04-19 12:24:09 +0530

    cartReducer added

--- src/State/Product/Action.js
+++ src/State/Product/Action.js
@@ -23,7 +23,7 @@ export const findProducts = (reqData) => async (dispatch) => {
     pageSize,
   } = reqData;
   try {
-    const { data } = api.get(
+    const { data } = await api.get(
       `/api/products/color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
     );
     dispatch({ type: FIND_PRODUCTS_SUCCESS, payload: data });
@@ -36,7 +36,7 @@ export const findProductsById = (reqData) => async (dispatch) => {
   dispatch({ type: FIND_PRODUCT_BY_ID_REQUEST });
   const { productId } = reqData;
   try {
-    const { data } = api.get(`/api/products/id/${productId}`);
+    const { data } = await api.get(`/api/products/id/${productId}`);
     dispatch({ type: FIND_PRODUCT_BY_ID_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: FIND_PRODUCT_BY_ID_FAILURE, payload: error.message });
